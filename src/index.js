b1a1384f5b9c4955b7bfcba6e976c30b0d1fffc0
Agustin <agus.delmonti@gmail.com>
2019-12-11 20:57:48 -0300

    Fix root component rendering

--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
-import MapView from "./pages/GoogleMaps";
-import Login from "./pages/login.js";
-import SignUp from "./pages/signup";
-import Interest from "./pages/interests";
-import Trip from "./pages/trip";
-import NotFoundPage from "./pages/404";
 import LandingPage from "./pages/LandingPage";
 
-//ReactDOM.render(<Login />, document.getElementById("root"));
-//ReactDOM.render(<SignUp />, document.getElementById("root"));
-//ReactDOM.render(<MapView />, document.getElementById("root"));
-//ReactDOM.render(<Interest />, document.getElementById("root"));
-//ReactDOM.render(<Trip/>, document.getElementById("root"));
-//ReactDOM.render(<NotFoundPage />, document.getElementById("root"));
 ReactDOM.render(<LandingPage />, document.getElementById("root"));
