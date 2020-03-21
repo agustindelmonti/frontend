86fd7e9fd40d7355162d303b005d24dffc3a0a4f
lucasrandisi <lucasrandisi@gmail.com>
2020-03-20 21:30:40 -0300

    Ocultar navbar si no se está loggeado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,106 +12,55 @@ const Navbar = () => {
   //Authenticated prop provided by context
   const { authenticated, actions } = useContext(UserContext);
 
-  return (
-    <Header>
-      <Wrapper>
-        <Navigation>
-          <Logo />
+  if(authenticated){
+    return (
+      <Header className="row no-gutters">
+        <Wrapper className="col-12 d-flex justify-content-end">
+          <Navigation>
+            <Logo />
 
-          <StyledLink to={"/home"}>Descubrir</StyledLink>
+            <StyledLink to={"/home"}>Descubrir</StyledLink>
 
-          <StyledLink to={"/"}>Explorar</StyledLink>
+            <StyledLink to={"/"}>Explorar</StyledLink>
 
-          <StyledLink to={"/trip"}>Viajes</StyledLink>
-        </Navigation>
+            <StyledLink to={"/trip"}>Viajes</StyledLink>
+          </Navigation>
 
-        <SearchBox />
+          <SearchBox />
 
-        <Actions>
-          {!authenticated && (
-            <Link to={"/login"}>
-              <button>Log In</button>
-            </Link>
-          )}
-          {authenticated && <Dropdown />}
-        </Actions>
-      </Wrapper>
-    </Header>
-  );
+          <Actions>
+            {!authenticated && (
+              <Link to={"/login"}>
+                <button>Log In</button>
+              </Link>
+            )}
+            {authenticated && <Dropdown />}
+          </Actions>
+        </Wrapper>
+      </Header>
+    );
+  }
+  else{
+    return null;
+  }
 };
 
 //export default withUser(Navbar);
 export default Navbar;
 
 const Header = styled.div`
-  -ms-flex-align: center;
-  align-items: center;
-  display: -ms-inline-flexbox;
-  display: inline-flex;
-  box-sizing: border-box;
-  -ms-flex-direction: row;
-  flex-direction: row;
-  -ms-flex-positive: 1;
-  flex-grow: 1;
 
-  left: 0;
-  position: fixed;
-  right: 0;
-  top: 0;
-  z-index: 80;
-  margin: 0;
-  height: 48px;
-
-  && {
-    background: rgb(240, 240, 240);
-  }
 `;
 
 const Wrapper = styled.div`
-  -ms-flex-align: center;
-  align-items: center;
-  display: -ms-inline-flexbox;
-  display: inline-flex;
-
-  background-color: var(--newCommunityTheme-body);
-  border-bottom: 1px solid var(--newCommunityTheme-line);
-  box-sizing: border-box;
-  -ms-flex-direction: row;
-  flex-direction: row;
-  -ms-flex-positive: 1;
-  flex-grow: 1;
-  padding: 0 20px;
 `;
 
 const Navigation = styled.nav`
-  align-items: center;
-  display: -ms-inline-flexbox;
-  display: inline-flex;
-  -ms-flex-direction: row;
-  flex-direction: row;
-  -ms-flex-positive: 1;
-  flex-grow: 1;
-
-  /*
-  @media (min-width: 1280px) {
-    max-width: 1200px;
-  }
-  */
 `;
 
 const Actions = styled.div`
-  -ms-flex-align: center;
-  align-items: center;
-  display: -ms-inline-flexbox;
-  display: inline-flex;
-  -ms-flex-direction: row;
-  flex-direction: row;
-  -ms-flex-positive: 0;
-  flex-grow: 0;
+
 `;
 
 const StyledLink = styled(Link)`
-  color: black;
-  text-decoration: none;
-  padding: 5px;
 `;
